1673e015dc3b7fe76e09a2e42c28d4f242e5674b
Karen Kraft <kraftkj@comcast.net>
2018-08-02 14:07:09 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,9 +1,21 @@
 require_relative 'config/environment'
+require_relative 'models/model.rb' 
 
 class App < Sinatra::Base
 
-  get '/' do 
-    "Welcome to your app!!!! "
+  get '/' do
+    erb :feminism_form
   end
 
-end
+
+  post '/' do
+    @myresult = feminism_quiz(params[:question1])
+    erb :results
+  end
+  
+  post '/' do
+    @myresult = feminism_quiz(params[:question2])
+    erb :results
+  end 
+end 
+  # Add your post route and action below
---
+++ models/model.rb
@@ -0,0 +1,17 @@
+# def feminism_quiz(question1)
+  
+#   if question1 == "answer2"
+#     return "Correct! Awesome job!"
+#   else
+#     return "It's okay to be wrong! You are not the only one."
+#   end 
+
+# end
+
+def feminism_quiz(question1, question2, question3)
+  if question1="answer2"
+    return "correct"
+  else return "sorry"
+  end 
+end 
+
