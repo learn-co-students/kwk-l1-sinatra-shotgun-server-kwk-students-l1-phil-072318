4dd865ed466bb44e0f6ad0afbf0f02045d4fc09f
Karen Kraft <kraftkj@comcast.net>
2018-08-02 18:06:29 +0000

    Done.

--- app.rb
+++ app.rb
@@ -4,14 +4,41 @@ require_relative 'models/model.rb'
 class App < Sinatra::Base
 
   get '/' do
-    erb :feminism_form
+    erb :index
   end
 
-  post '/' do
+  get '/feminism_quiz' do 
+    erb :feminism_form
+  end 
+
+  post '/feminism_quiz' do
     @myresult = feminism_quiz_question1(params[:question1])
     @myresult = feminism_quiz_question2(params[:question2])
     @myresult = feminism_quiz_question3(params[:question1])
     erb :results
   end 
+  
+  get '/equality_quiz' do 
+    erb :equality_form
+  end 
+
+  post '/equality_quiz' do
+    @myresult = equality_quiz_question1(params[:question1])
+    @myresult = equality_quiz_question2(params[:question2])
+    @myresult = equality_quiz_question3(params[:question3])
+    erb :results
+  end 
+  
+    get '/gun_control_quiz' do 
+    erb :gun_control_form
+  end 
+
+  post '/gun_control_quiz' do
+    @myresult = guncontrol_quiz_question1(params[:question1])
+    @myresult = guncontrol_quiz_question2(params[:question2])
+    @myresult = guncontrol_quiz_question3(params[:question3])
+    erb :results
+  end 
+  
 end 
   # Add your post route and action below
--- models/model.rb
+++ models/model.rb
@@ -1,13 +1,3 @@
-# def feminism_quiz(question1)
-  
-#   if question1 == "answer2"
-#     return "Correct! Awesome job!"
-#   else
-#     return "It's okay to be wrong! You are not the only one."
-#   end 
-
-# end
-
 @@results = []
 
 def feminism_quiz_question1(answer)
@@ -27,11 +17,61 @@ def feminism_quiz_question2(answer)
 end 
 
 def feminism_quiz_question3(answer)
-  if answer == "answer1"
+  if answer == "answer2"
     @@results.push "AWesome"
   else 
     @@results.push "oops"
   end 
 end 
+
+  def equality_quiz_question1(answer)
+  if answer == "answer1"
+    @@results.push("AWesome")
+  else
+    @@results.push("oops")
+  end 
+end 
+
+def equality_quiz_question2(answer)
+  if answer == "answer3"
+    @@results.push("AWesome")
+  else 
+    @@results.push("oops")
+  end 
+end 
+
+def equality_quiz_question3(answer)
+  if answer == "answer2"
+    @@results.push("AWesome")
+  else 
+    @@results.push("oops")
+  end 
+end 
+
+def guncontrol_quiz_question1(answer)
+ if answer == "answer3"
+    @@results.push("AWesome")
+  else 
+    @@results.push("oops")
+  end 
+end 
+
+def guncontrol_quiz_question2(answer)
+  if answer == "answer2"
+    @@results.push("AWesome")
+  else 
+    @@results.push("oops")
+  end 
+end 
+
+def guncontrol_quiz_question3(answer)
+  if answer == "answer2"
+    @@results.push("AWesome")
+  else 
+    @@results.push("oops")
+  end 
+end 
+    
+ 
     
 
